08db675e39eaa0de5b5394de6f41bbeca5621422
FTI\brian.schoolcraft <brian.p.schoolcraft@fallbrooktech.com>
2018-03-12 10:35:44 -0500

    updates...?

--- strava_ultimate.py
+++ strava_ultimate.py
@@ -19,7 +19,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
 
 
-# In[2]:
+# In[3]:
 
 #### Setting up strava API client
 
@@ -38,23 +38,9 @@ display(HTML("""<a href="{}">{}</a>""".format(auth_url,auth_url)))
 code = '6d057263b427852b0489af26e921f8fd25a78852'
 access_token = client.exchange_code_for_token(client_id=19435, client_secret='45b776d5beceeb34c290b8a56bf9829d6d4ea5d7', code=code)
 
-client = stravalib.client.Client(access_token=access_token)
-athlete = client.get_athlete()
-print('athlete name %s, athlete id %s.' %(athlete.firstname, athlete.id))
-
-
-# In[3]:
-
-### Setting up google sheets client -- old library
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-
-
-# # use creds to create a client to interact with the Google Drive API
-# scope = ['https://spreadsheets.google.com/feeds']
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-# gspread_client = gspread.authorize(creds)
- 
+strava_client = stravalib.client.Client(access_token=access_token)
+athlete = strava_client.get_athlete()
+print('athlete name %s, athlete id %s, dob: %s.' %(athlete.firstname, athlete.id, athlete.dateofbirth))
 
 
 # In[4]:
@@ -69,27 +55,30 @@ sh = gc.open('Milburn Ultimate Scores')
 wks = sh.sheet1
 
 
-# In[9]:
+# In[14]:
 
 ## Get last entry from Data Spreadsheet
 
 dates_recorded = [datetime.strptime(d, '%Y-%m-%d') for d in wks.get_col(1) if d != '' and d != 'Date']
 lap_start_date = max(dates_recorded) + timedelta(days=1)
-dates_recorded, lap_start_date
-
-
-# In[10]:
-
-runs = []
-for activity in client.get_activities(after=lap_start_date):
-    if 'ltimate' in activity.name and activity.type == 'Run':
-        runs.append(activity)
+# lap_start_date = dates_recorded[20]
+lap_start_date
+# from datetime import timedelta
+# lap_start_date = lap_start_date - timedelta(days=2)
 
 
 # In[15]:
 
-def get_strava_description(activity):
-    new_activity = client.get_activity(activity.id)
+runs = []
+for activity in strava_client.get_activities(after=lap_start_date):
+    if 'ltimate' in activity.name and activity.type == 'Run':
+        runs.append(activity)
+
+
+# In[16]:
+
+def get_strava_description(activity, p=False):
+    new_activity = strava_client.get_activity(activity.id)
     try:
         scores, color = new_activity.description.split(' ')
         try:
@@ -105,10 +94,15 @@ def get_strava_description(activity):
     except AttributeError:
         color = None
         
+    if p:
+        print(new_activity.start_date)
+        print(new_activity.description)
+        print(team_score, opponent_score, color)
+        
     return team_score, opponent_score, color
 
 
-# In[16]:
+# In[17]:
 
 def extract_events(run):
     lap_nums = []
@@ -134,6 +128,7 @@ def process_events(events):
         'team_point',
         'opponent_point',
         'my_point',
+        'game',
         'game',
         'game'
     ]
@@ -185,36 +180,47 @@ def process_events(events):
     return games
 
 
-# In[ ]:
+# In[9]:
 
 games = []
 for run in runs:
-    team_score, opponent_score, color = get_strava_description(run)
-    print(run.start_date_local, get_strava_description(run))
+    team_wins, opponent_wins, color = get_strava_description(run, p=True)
+    
     events = extract_events(run)
+    
+    for e in events:
+        print(e)
     current_games = process_events(events)
+    
     for g in current_games:
         g['my_color'] = color
+        g['team_wins'] = team_wins
+        g['opponent_wins'] = opponent_wins
+        
 
     games = games + current_games
     
 
 
-# In[ ]:
+# In[10]:
 
-df = pd.DataFrame(games)
+df = pd.DataFrame(games).dropna()
+
+
+# In[11]:
+
 df['date'] = df.end_time.apply(lambda x: date(x.year, x.month, x.day))
 
 df = df.set_index(['date', 'game_num'], drop=False)
 df
 
 
-# In[10]:
+# In[11]:
 
 pdf = df
 
 
-# In[11]:
+# In[12]:
 
 pdf['white_wins'] = None
 pdf['color_wins'] = None
@@ -223,17 +229,17 @@ pdf['color_point'] = None
 pdf['game_winner'] = None
 
 
-# In[12]:
+# In[13]:
 
 for (date, game_num), row in pdf.iterrows():      
     
-    if 'white' in row.my_color:
+    if row.my_color == 'white':
         if row.win:
             pdf.loc[(date, game_num), 'game_winner'] = 'White'
         else:
             pdf.loc[(date, game_num), 'game_winner'] = 'Color'
-        pdf.loc[(date, game_num), 'white_wins'] = team_score                
-        pdf.loc[(date, game_num), 'color_wins'] = opponent_score   
+        pdf.loc[(date, game_num), 'white_wins'] = pdf.loc[(date, game_num), 'team_wins']            
+        pdf.loc[(date, game_num), 'color_wins'] = pdf.loc[(date, game_num), 'opponent_wins']            
         pdf.loc[(date, game_num), 'white_point'] = pdf.loc[(date, game_num), 'team_point']                
         pdf.loc[(date, game_num), 'color_point'] = pdf.loc[(date, game_num), 'opponent_point']
 
@@ -242,23 +248,18 @@ for (date, game_num), row in pdf.iterrows():
             pdf.loc[(date, game_num), 'game_winner'] = 'White'
         else:
             pdf.loc[(date, game_num), 'game_winner'] = 'Color'
-        pdf.loc[(date, game_num), 'color_wins'] = team_score                
-        pdf.loc[(date, game_num), 'white_wins'] = opponent_score   
+        pdf.loc[(date, game_num), 'color_wins'] = pdf.loc[(date, game_num), 'team_wins']            
+        pdf.loc[(date, game_num), 'white_wins'] = pdf.loc[(date, game_num), 'opponent_wins']    
         pdf.loc[(date, game_num), 'color_point'] = pdf.loc[(date, game_num), 'team_point']                
         pdf.loc[(date, game_num), 'white_point'] = pdf.loc[(date, game_num), 'opponent_point']
 
 
-# In[13]:
+# In[14]:
 
 pdf
 
 
-# In[40]:
-
-out_df = pdf[['white_wins', 'color_wins', 'game_num', 'game_winner']].copy()
-
-
-# In[50]:
+# In[15]:
 
 def merge_two_dicts(x, y):
     """Given two dicts, merge them into a new dict as a shallow copy."""
@@ -267,12 +268,7 @@ def merge_two_dicts(x, y):
     return z
 
 
-# In[ ]:
-
-
-
-
-# In[57]:
+# In[16]:
 
 scores = []
 for (date, game_num), game in pdf.iterrows():
@@ -283,19 +279,26 @@ for (date, game_num), game in pdf.iterrows():
 score_df = pd.DataFrame(scores).set_index(['date', 'game_num', 'team'], drop=False)
 
 
-# In[63]:
+# In[17]:
 
-out_matrix = score_df[['date', 'white_wins', 'color_wins', 'game_num', 'game_winner', 'team_score', 'my_score']].sort_index(ascending=False, level=0).as_matrix()
-
-
-# In[65]:
-
-out_matrix.shape
+out_df = score_df[['date', 'white_wins', 'color_wins', 'game_num', 'game_winner', 'team', 'team_score', 'my_score']].sort_index(ascending=False, level=0).fillna(value='')
+out_df['date'] = out_df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
 
-# In[69]:
+# In[18]:
 
-sheet.insert_row(out_matrix[0,:], 3)
+out_df
+
+
+# In[19]:
+
+data = out_df.as_matrix().tolist()
+wks.insert_rows(2, values=data, number=len(data))
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
